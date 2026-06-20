9ad3708ae034999507e00cd06070c412976d474e
Aditya <adityaj02810@gmail.com>
2026-06-20 20:10:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useMemo, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px', fontSize: '24px', color: '#333' }}>
+      <img src="https://i.pinimg.com/736x/54/7a/7f/547a7f9693b6ab79efcd963d2d760fcf.jpg" alt="Cirqmon Logo" style={{ width: '150px', textAlign: 'center', marginBottom: '20px',margin: '0 auto' }} />
+     <h1>Welcome to Cirqmon</h1>
+     <p style={{ fontSize: '18px', color: '#666' }}>Wait for it...</p>
+    </div>
+
+  )
+}
+
+export default App
+  
